30256de4a0d6cde164cda44947b149ffd35b4de6
Andrelo9 <andres.vaninetti@globant.com>
2017-07-31 16:22:18 -0300

    Andrelo - CR Fixes - Change model and runned in sql workbench

--- MySqlFiles/HighSchool.sql
+++ MySqlFiles/HighSchool.sql
@@ -1,8 +1,9 @@
-CREATE DATABASE high_school;
+						/* Exercise 1 */
+CREATE DATABASE IF NOT EXISTS high_school;
+USE high_school;
 
 CREATE TABLE IF NOT EXISTS student(
-	registration_number int NOT NULL PRIMARY KEY,
-    dni_number char(8) UNIQUE NOT NULL,
+	registration_number int NOT NULL PRIMARY KEY,    
     first_name varchar(255) NOT NULL,
     last_name varchar(255) NOT NULL,	
     birth_date date
@@ -18,30 +19,40 @@ CREATE TABLE IF NOT EXISTS teacher(
 CREATE TABLE IF NOT EXISTS course(
 	course_id int NOT NULL PRIMARY KEY,
     course_name varchar(255) NOT NULL,
-    assigned_teacher varchar(255),
+    teacher_id int,
 	hours_by_week time,   
-    CONSTRAINT assigned_teacher_fk FOREIGN KEY(assigned_teacher)
-    REFERENCES teacher(teacher_id),
-    CONSTRAINT students_fk FOREIGN KEY(assigned_course)
-    REFERENCES course(course_id)
+    CONSTRAINT assigned_teacher_fk FOREIGN KEY(teacher_id)
+    REFERENCES teacher(teacher_id)   
 );
 
-CREATE TABLE IF NOT EXISTS notes_history(
+CREATE TABLE IF NOT EXISTS inscript(
+	registration_number int,
+    course_id int,
+    PRIMARY KEY(registration_number,course_id),
+    CONSTRAINT registrationNumber_fk
+    FOREIGN KEY(registration_number) REFERENCES student(registration_number),
+    CONSTRAINT courseId_fk
+    FOREIGN KEY(course_id) REFERENCES course(course_id)
+    );
+
+CREATE TABLE IF NOT EXISTS historyNotes(	
 	first_note numeric,
     second_note numeric,
     third_note numeric,
     final_note numeric,
-    student_id int,
+    registration_number int,
     course_id int,
-    CONSTRAINT student_notes_fk 
-    FOREIGN KEY(student_id,course_id)
-    REFERENCES course(student_id,course_id)
+    CONSTRAINT student_notes_fk
+    FOREIGN KEY(registration_number)
+    REFERENCES student(registration_number)
 );
 
-CREATE TABLE IF NOT EXISTS course_calendar(	
+CREATE TABLE IF NOT EXISTS courseCalendar(	
 	schedule_time timestamp,
 	course_id int,    
     CONSTRAINT course_calendar_fk
     FOREIGN KEY(course_id)
     REFERENCES course(course_id)
 );
+
+					    /* Exercise 2 */
