efc6d81da752f70e2215755e2667b296cd033601
Andrelo9 <andres.vaninetti@globant.com>
2017-08-02 17:49:23 -0300

    Andrelo - Exercise 5 (not complete) -

--- MySqlFiles/HighSchool.sql
+++ MySqlFiles/HighSchool.sql
@@ -15,16 +15,16 @@ CREATE TABLE IF NOT EXISTS teacher(
 	teacher_id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,	
-    birth_date DATE
+    birth_date DATE    
 );
 
 CREATE TABLE IF NOT EXISTS course(
 	course_id INT NOT NULL PRIMARY KEY,
     course_name VARCHAR(255) NOT NULL,
     teacher_id INT,
-	hours_by_week TIME,   
+	hours_by_week TIME,
     CONSTRAINT assigned_teacher_fk FOREIGN KEY(teacher_id)
-    REFERENCES teacher(teacher_id)   
+    REFERENCES teacher(teacher_id)
 );
 
 CREATE TABLE IF NOT EXISTS inscript(
@@ -37,26 +37,30 @@ CREATE TABLE IF NOT EXISTS inscript(
     FOREIGN KEY(course_id) REFERENCES course(course_id)
     );
 
-CREATE TABLE IF NOT EXISTS historyNotes(	
-	first_note NUMERIC,
-    second_note NUMERIC,
-    third_note NUMERIC,
-    final_note NUMERIC,
-    registration_number INT,
-    course_id INT,
-    CONSTRAINT student_notes_fk
-    FOREIGN KEY(registration_number)
-    REFERENCES student(registration_number)
+CREATE TABLE IF NOT EXISTS historyNotes(
+	registration_number INT NOT NULL,
+    course_id INT NOT NULL,
+	first_note FLOAT,
+    second_note FLOAT,
+    third_note FLOAT,
+    final_note FLOAT,
+    PRIMARY KEY(registration_number,course_id)  
 );
 
-CREATE TABLE IF NOT EXISTS courseCalendar(	
-	schedule_time TIMESTAMP,
+CREATE TABLE IF NOT EXISTS courseCalendar(
+	course_calendar_id INT NOT NULL,
+	days_onWeek VARCHAR(255),	
+	schedule_Initial_time TIME,
+    schedule_final_time TIME,
 	course_id INT,    
+    PRIMARY KEY(course_calendar_id),
     CONSTRAINT course_calendar_fk
     FOREIGN KEY(course_id)
     REFERENCES course(course_id)
 );
 SET SQL_SAFE_UPDATES = 0;
+DELETE FROM coursecalendar;
+DELETE FROM historyNotes;
 DELETE FROM inscript;
 DELETE FROM student;
 DELETE FROM course;
@@ -68,7 +72,7 @@ INSERT INTO teacher VALUES (1, 'Matias', 'Massigogge', '1980-05-06');
 INSERT INTO teacher VALUES (2, 'Seba', 'Garofalo', '1980-06-06');
 INSERT INTO teacher VALUES (3, 'Cristian', 'Ebbens', '1980-05-06');
 
-						/* Information for 3 students*/
+						/* Information for 3 courses */
 INSERT INTO course VALUES (100, 'algoritmos', 2, '06:00:00');
 INSERT INTO course VALUES (101, 'bases de datos', 1, '06:00:00');
 INSERT INTO course VALUES (102, 'diseño', 3, '06:00:00');
@@ -118,6 +122,23 @@ INSERT INTO inscript VALUES (1006,102);
 INSERT INTO inscript VALUES (1007,102);
 INSERT INTO inscript VALUES (1008,102);
 INSERT INTO inscript VALUES (1009,102);
+		        
+					/* History notes for 3 students */
+INSERT INTO historyNotes VALUES (1000,100,7,8,9,6);
+INSERT INTO historyNotes VALUES (1001,100,4,5,2,7);
+INSERT INTO historyNotes VALUES (1002,100,2,4,2,2);
+
+					/* Set schedules */
+INSERT INTO coursecalendar VALUES (30,'Monday', '10:00:00','13:00:00',101);
+INSERT INTO coursecalendar VALUES (31,'Wednesday', '16:00:00','18:00:00',101);
+INSERT INTO coursecalendar VALUES (32,'Friday', '12:00:00','14:00:00',101);
+INSERT INTO coursecalendar VALUES (33,'Monday'	, '10:00:00','11:00:00',100);
+INSERT INTO coursecalendar VALUES (34,'Thursday', '16:00:00','19:00:00',100);
+INSERT INTO coursecalendar VALUES (35,'Friday', '12:00:00','16:00:00',100);
+INSERT INTO coursecalendar VALUES (36,'Sunday'	, '10:00:00','12:00:00',102);
+INSERT INTO coursecalendar VALUES (37,'Saturday', '16:00:00','20:00:00',102);
+INSERT INTO coursecalendar VALUES (38,'Friday', '12:00:00','12:30:00',102);
+
 
 					/* EXERCISE 3 */
 SELECT DISTINCT course.course_name AS 'COURSE',
@@ -131,3 +152,17 @@ FROM course
 	INNER JOIN student ON inscript.registration_number = student.registration_number 
 WHERE inscript.course_id = "101"
 ORDER BY student.last_name;
+
+					/* EXERCISE 4 %PASSED and %FAILED */
+SELECT AVG(final_note) AS 'TOTAL_PASSED_AVERAGE' FROM historyNotes
+WHERE final_note > 4 AND course_id = 100;
+
+SELECT AVG(final_note) AS 'TOTAL_FAILED_AVERAGE' FROM historyNotes
+WHERE final_note < 4 AND course_id = 100;
+					
+                    /* EXERCISE 5 */
+SELECT coursecalendar.course_id, coursecalendar.days_onWeek,
+	    coursecalendar.schedule_Initial_time,coursecalendar.schedule_final_time
+FROM coursecalendar			
+WHERE coursecalendar.course_id = 100
+ORDER BY coursecalendar.days_onWeek;
