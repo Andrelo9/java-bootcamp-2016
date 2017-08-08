baf47752cf87725cb2f7b6670134022add1093ed
Andrelo9 <andres.vaninetti@globant.com>
2017-08-08 14:38:24 -0300

    Andrelo - Topic 5 - JSON db files for perform with mongoDB shell

--- MavenQuickStart/quickApp/src/main/java/com/globant/topicfive/Main.java
+++ MavenQuickStart/quickApp/src/main/java/com/globant/topicfive/Main.java
@@ -4,6 +4,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 public class Main {
@@ -13,13 +14,16 @@ public class Main {
 		try {
 			// To connect to mongodb server
 			MongoClient mongoClient = new MongoClient("localhost", 27017);
-			DB database = mongoClient.getDB("HighSchool");
-			DBCollection coll = (DBCollection) database.getCollection("highSchoolCollection");		
+			DB database = mongoClient.getDB("high-school");			
+			DBCollection student = (DBCollection) database.getCollection("highSchool");
+			
 			BasicDBObject doc = new BasicDBObject("student","MongoDB")
+					.append("registration_number_id", 1)
 					.append("firstName", "Andres")
-					.append("lastName", "Vaninetti");
-			coll.insert(doc);
-			DBCursor cursor = coll.find();
+					.append("lastName", "Vaninetti")
+					.append("birth_date", "1986-12-18");			
+			student.insert(doc);	
+			DBCursor cursor = student.find();
 	         int i = 1;
 				
 	         while (cursor.hasNext()) { 
@@ -30,6 +34,5 @@ public class Main {
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 		}
-	}
-
+	}	
 }
