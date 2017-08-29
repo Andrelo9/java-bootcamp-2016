e48b0bc51f18b41fc277091546313e8c7875b86e
Andrelo9 <andres.vaninetti@globant.com>
2017-08-29 17:19:31 -0300

    Andrelo

--- Topics/Implementations/src/main/java/com/globant/finalproject/securityconfiguration/WebSecurityConfig.java
+++ Topics/Implementations/src/main/java/com/globant/finalproject/securityconfiguration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.globant.finalproject.securityconfiguration;
 
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfiguration {
 	
+	private static final String URL_LOGOUT = "http://localhost:8080/shoppingCart/logout";
 	private static final String ROLE = "USER";
 	private static final String PASSWORD = "password";
 	private static final String NAME = "name";
@@ -26,4 +28,17 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
 		return manager;
 	}
 	
+	protected void configure(HttpSecurity http) throws Exception {		
+		 		http
+		 			.logout()
+		 				.logoutUrl(URL_LOGOUT)
+		 				.invalidateHttpSession(true)
+		 			.and()
+		 			.authorizeRequests()
+		 				.anyRequest().authenticated()
+		 				.and()
+		 			.formLogin()
+		 				.and()
+		 			.httpBasic();
+		 	}	
 }
